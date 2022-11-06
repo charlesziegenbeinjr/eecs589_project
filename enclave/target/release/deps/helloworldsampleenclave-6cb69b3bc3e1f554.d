5078a806dc3b65ed219b17bea60b38eeb97ca9be
Charles Ziegenbein <charles.ziegenbein@gmail.com>
2022-11-06 20:40:03 +0000

    adding hello-rust starter code

---
+++ enclave/target/release/deps/helloworldsampleenclave-6cb69b3bc3e1f554.d
@@ -0,0 +1,5 @@
+/root/sgx/samplecode/hello-rust/enclave/target/release/deps/libhelloworldsampleenclave-6cb69b3bc3e1f554.a: src/lib.rs
+
+/root/sgx/samplecode/hello-rust/enclave/target/release/deps/helloworldsampleenclave-6cb69b3bc3e1f554.d: src/lib.rs
+
+src/lib.rs:
